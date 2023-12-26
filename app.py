5811ec170cf3cef76b00808dcec88401e6e67f1f
Sai336606 <129481966+Sai336606@users.noreply.github.com>
2023-12-26 17:37:26 +0000

    Added Dynamic data and API Route
    Added the Dynamic data in the app.py file and that is displayed in the index.html using jinja template code i.e {% loop %} etc...
    
    added API Route in the app.py file using jsonify

--- app.py
+++ app.py
@@ -1,11 +1,47 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+# now the Data is adding in this variable
+Jobs = [
+  {
+    'id': 1,
+    'title': 'Python Developer',
+    'company': 'Google',
+    'location': 'Banglore',
+    'salary': '10LPA'
+  },
+  {
+    'id': 2,
+      'title': 'Front-end Developer',
+      'company': 'Meta',
+      'location': 'usa',
+      'salary': '$120,000'
+    },
+  {
+  'id': 3,
+    'title': 'Back-End Developer',
+    'company': 'Volvo',
+    'location': 'India',
+    'salary': '12 lpa'
+  },
+  {
+  'id': 4,
+    'title': 'AI Developer',
+    'company': 'Open Ai',
+    'location': 'usa',
+    # 'salary': '$180,000'
+  }
+]
 @app.route('/')
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html",jobs=Jobs,company='Mostly AI')
 
+
+# api route using json alternative to HTML 
+@app.route('/api/jobs')
+def jobs():
+  return jsonify(Jobs)
 
 if __name__ == "__main__":
   app.run('0.0.0.0',debug=True)
