d7e8f07738304630cd87d553467e67d5cdadf687
MohanSai <mohansaik89@gmail.com>
2024-02-01 15:32:39 +0530

    added the Nav_bar  in the home_page from  nav.html and mailto link for the apply button

--- app.py
+++ app.py
@@ -4,44 +4,51 @@ app = Flask(__name__)
 
 # now the Data is adding in this variable
 Jobs = [
-  {
-    'id': 1,
-    'title': 'Python Developer',
-    'company': 'Google',
-    'location': 'Banglore',
-    'salary': '10LPA'
-  },
-  {
-    'id': 2,
-      'title': 'Front-end Developer',
-      'company': 'Meta',
-      'location': 'usa',
-      'salary': '$120,000'
+    {
+        "id": 1,
+        "title": "Python Developer",
+        "company": "Google",
+        "location": "Banglore",
+        "salary": "10 LPA",
+        "Experience": "7-9 years",
     },
-  {
-  'id': 3,
-    'title': 'Back-End Developer',
-    'company': 'Volvo',
-    'location': 'India',
-    'salary': '12 lpa'
-  },
-  {
-  'id': 4,
-    'title': 'AI Developer',
-    'company': 'Open Ai',
-    'location': 'usa',
-    # 'salary': '$180,000'
-  }
+    {
+        "id": 2,
+        "title": "Front-end Developer",
+        "company": "Meta",
+        "location": "usa",
+        "salary": "$120,000",
+        "Experience": "5-7 years",
+    },
+    {
+        "id": 3,
+        "title": "Back-End Developer",
+        "company": "Volvo",
+        "location": "India",
+        "salary": "12 lpa",
+        "Experience": "7.5 years",
+    },
+    {
+        "id": 4,
+        "title": "AI Developer",
+        "company": "Open Ai",
+        "location": "usa",
+        # 'salary': '$180,000',
+        "Experience": "9 years",
+    },
 ]
-@app.route('/')
+
+
+@app.route("/")
 def hello_world():
-  return render_template("home.html",jobs=Jobs,company='Mostly AI')
+    return render_template("home.html", jobs=Jobs, company="AI-Most")
 
 
-# api route using json alternative to HTML 
-@app.route('/api/jobs')
+# api route using json alternative to HTML
+@app.route("/api/jobs")
 def jobs():
-  return jsonify(Jobs)
+    return jsonify(Jobs)
+
 
 if __name__ == "__main__":
-  app.run('0.0.0.0',debug=True)
+    app.run("0.0.0.0", debug=True)
